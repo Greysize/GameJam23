482e588d4c25d6e04456a7e75ebebcf6fde566b4
Greysize <Greysize@gmail.com>
2023-10-28 19:37:17 +0100

    [ADD] changed controller for first person

---
+++ Assets/Invector-3rdPersonController_LITE/Scripts/Interactions/Interactor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    public bool canInteract = false;
+    public string InteractButton = "Fire1";
+
+    private bool TestInteraction = false;
+    private void Update()
+    {
+       // TestInteraction = GetKey(InteractButton);
+    }
+}
