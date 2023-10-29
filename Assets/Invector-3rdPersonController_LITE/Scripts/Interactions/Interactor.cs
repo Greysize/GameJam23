53d184b19b5fd0ddfd7821b4029ea5ff250fe1e1
Greysize <Greysize@gmail.com>
2023-10-29 10:11:27 +0000

    Finish

--- Assets/Invector-3rdPersonController_LITE/Scripts/Interactions/Interactor.cs
+++ Assets/Invector-3rdPersonController_LITE/Scripts/Interactions/Interactor.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 
 public class Interactor : MonoBehaviour
 {
+    public Transform InteractorOrigin;
     public bool canInteract = false;
+    public float Raydistance = 2f;
     public string InteractButton = "Fire1";
 
-    private bool TestInteraction = false;
+    private bool isTestNeeded = false;
     private void Update()
     {
-       // TestInteraction = GetKey(InteractButton);
+        if (Input.GetButtonDown(InteractButton))
+            InteractionTest();
+    }
+
+    private void InteractionTest()
+    {
+        Ray rayInteract = new Ray(InteractorOrigin.transform.position, InteractorOrigin.transform.forward);
+        RaycastHit hitData;
+        Debug.DrawRay(InteractorOrigin.transform.position, InteractorOrigin.transform.forward, Color.red);
+
+        if (Physics.Raycast(rayInteract, out hitData, Raydistance))
+        {
+            if(hitData.collider.gameObject.GetComponent<Interactable>())
+            {
+                hitData.collider.gameObject.GetComponent<Interactable>().isUsed = true;
+            }
+        }
     }
 }
