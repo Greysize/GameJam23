65fd7f71d741b749c26a9a9f657e67aa31e43079
PaulTorris <paultorris@gmail.com>
2023-10-29 20:11:51 +0000

    fkybox fix

--- Assets/Scripts/SceneLoader/ScenesLoader.cs
+++ Assets/Scripts/SceneLoader/ScenesLoader.cs
@@ -5,13 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class ScenesLoader : MonoBehaviour
 {
+    Scene thisScene;
     // Start is called before the first frame update
     void Start()
     {
+        thisScene = SceneManager.GetActiveScene();
+        print ("SCENE NAME : " + thisScene.name);
         SceneManager.LoadScene("Level1_part1_Jeremi", LoadSceneMode.Additive);
         SceneManager.LoadScene("Level1_part2_Pierre", LoadSceneMode.Additive);
         SceneManager.LoadScene("Level1_part3_Nico", LoadSceneMode.Additive);
-
+        SceneManager.SetActiveScene(thisScene);
+        StartCoroutine(ForceActiveScene());
     }
 
+    private IEnumerator ForceActiveScene()
+    {
+        yield return new WaitForSeconds(0.3f);
+        print("force activeScene");
+        SceneManager.SetActiveScene(thisScene);
+        yield return null;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            SceneManager.SetActiveScene (thisScene);
+        }
+    }
 }
